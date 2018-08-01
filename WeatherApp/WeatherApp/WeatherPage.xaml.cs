9b88eea3bd5bf65387f08257621773f51ce98c27
Nigel Liggett <nigel.liggett@gmail.com>
2018-08-01 21:29:47 +0100

    Add project files.

---
+++ WeatherApp/WeatherApp/WeatherPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace WeatherApp
+{
+    public partial class WeatherPage : ContentPage
+    {
+        public WeatherPage()
+        {
+            InitializeComponent();
+
+            //Set the default binding to a default object for now
+            BindingContext = new Weather();
+        }
+
+        private async void GetWeatherBtn_Clicked(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(zipCodeEntry.Text))
+            {
+                Weather weather = await Core.GetWeather(zipCodeEntry.Text);
+                BindingContext = weather;
+                getWeatherBtn.Text = "Search Again";
+            }
+        }
+    }
+}
